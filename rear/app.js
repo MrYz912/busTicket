edf8ddfe5395d68eb7d5e42718fc40d0c578d99c
MrYz912 <1600332850@qq.com>
2021-05-04 15:14:01 +0800

    更新文档

--- rear/app.js
+++ rear/app.js
@@ -10,12 +10,13 @@ app.use(bodyParser())
 app.use(
   cors({
     origin: (ctx) =>{
-      const whiteList = ['http://localhost:9091','http://localhost:8080']; //可跨域白名单
-      let url = ctx.header.referer.substr(0,ctx.header.referer.length - 1);
-      if(whiteList.includes(url)){
-        return url //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
-      }
-      return 'http://localhost:3000' //默认允许本地请求3000端口可跨域
+      // const whiteList = ['http://localhost:9091','http://localhost:8080']; //可跨域白名单
+      // let url = ctx.header.referer.substr(0,ctx.header.referer.length - 1);
+      // if(whiteList.includes(url)){
+      //   return url //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
+      // }
+      // return 'http://localhost:3000' //默认允许本地请求3000端口可跨域
+      return '*';
     },
     maxAge: 5, //指定本次预检请求的有效期，单位为秒。
     credentials: true, //是否允许发送Cookie
--- rear/app/config/config.js
+++ rear/app/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'yangzhen123.'
+    password: 'root'
   },
   security: {
     secretKey: "R!7BWk*0CQ_SCC*behcXws7GtQhpY*Kh",
